ddf8da364be9704495fa13c643cd651c031bb899
qdeu22 <qdeu22@gmail.com>
2023-04-09 16:52:25 +0900

    feat: URL 직접젒고 차단

--- app.js
+++ app.js
@@ -72,12 +72,56 @@ app.get("/logout", isLoggedIn, function (req, res, next) {
   });
 });
 
-app.get("/channel", function (req, res, next) {
+app.get("/channel", isLoggedIn, function (req, res, next) {
   res.render("channel", {});
 });
 
-app.get("/:pageId", function (req, res, next) {
-  res.send(`HELLo`);
+app.get("/channel/:pageId", function (req, res, next) {
+  console.log("/channel/:pageId", req.headers.referer);
+  if (req.headers.referer !== "http://localhost:3000/channel") {
+    // 이전 페이지가 특정한 URL이 아닌 경우
+    return res.send(
+      `<script>
+         alert('잘못된 접근입니다.');
+         history.back();      
+      </script >`
+    );
+  }
+  var number = req.params.pageId;
+  res.render("channel_process", { number });
+});
+
+// 채널 1 혼잡도 정보 전송
+function sendChannel1Congestion() {
+  const congestion = Math.random(); // 임의의 혼잡도 정보 생성
+  io.emit("channel1_congestion", congestion);
+}
+
+// 채널 2 혼잡도 정보 전송
+function sendChannel2Congestion() {
+  const congestion = Math.random(); // 임의의 혼잡도 정보 생성
+  io.emit("channel2_congestion", congestion);
+}
+
+// 1초마다 혼잡도 정보 전송
+setInterval(() => {
+  sendChannel1Congestion();
+  sendChannel2Congestion();
+}, 1000);
+
+// Socket.io 연결
+io.on("connection", (socket) => {
+  console.log("새로운 클라이언트 연결됨");
+
+  // 클라이언트로부터 채널 1 요청 받음
+  socket.on("request_channel1", () => {
+    sendChannel1Congestion();
+  });
+
+  // 클라이언트로부터 채널 2 요청 받음
+  socket.on("request_channel2", () => {
+    sendChannel2Congestion();
+  });
 });
 
 server.listen(port, () => {
--- src/javascript/channel.js
+++ src/javascript/channel.js
@@ -1,1 +1,19 @@
 const socket = io();
+
+// 채널 1 요청
+socket.emit("request_channel1");
+
+// 채널 1 혼잡도 정보 수신
+socket.on("channel1_congestion", (congestion) => {
+  const channel1Elem = document.getElementById("channel1");
+  channel1Elem.innerHTML = `채널 1 혼잡도: ${congestion}`;
+});
+
+// 채널 2 요청
+socket.emit("request_channel2");
+
+// 채널 2 혼잡도 정보 수신
+socket.on("channel2_congestion", (congestion) => {
+  const channel2Elem = document.getElementById("channel2");
+  channel2Elem.innerHTML = `채널 2 혼잡도: ${congestion}`;
+});
