795e284e83591226c68d493201bd47889233b0e3
qdeu22 <qdeu22@gmail.com>
2023-04-09 14:41:20 +0900

    feat: 로그인 구현!!!

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+const io = socketIO(server);
+
+var passport = require("./lib/passport");
+var session = require("express-session");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(
+  //+
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(passport.initialize()); //+
+app.use(passport.session()); //+
+
+app.use(express.static(path.join(__dirname, "src")));
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.render("alert", {
+    message: "로그인이 필요합니다.",
+    url: "/login",
+  });
+}
+
+app.get("/", (req, res) => {
+  const title = "캐치마인드";
+  const isLoggedIn = !!req.user;
+  const username = req.user ? req.user.displayName : null;
+  res.render("index", { title, isLoggedIn, username });
+});
+
+app.get("/login", function (req, res, next) {
+  res.render("login", {});
+});
+
+app.get(
+  "/login/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/fail" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
+app.get("/logout", isLoggedIn, function (req, res, next) {
+  req.logout(function (err) {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
