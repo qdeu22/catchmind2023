267bb5bd63600c4e61e5531e0df0f3260da0c0b5
qdeu22 <qdeu22@gmail.com>
2023-04-29 19:51:28 +0900

    feat: 크기 이상으로 글 입력시 스크롤 내리고 자동으로 최근 글이 보도록함

--- src/javascript/chat.js
+++ src/javascript/chat.js
@@ -2,6 +2,10 @@ const form = document.querySelector("form");
 const input = document.querySelector('input[type="text"]');
 const ul = document.querySelector("ul");
 
+function scrollToBottom() {
+  ul.scrollTop = ul.scrollHeight;
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // 폼 기본 동작 방지
 
@@ -11,5 +15,7 @@ form.addEventListener("submit", (event) => {
     li.textContent = message; // 리스트 아이템에 메시지 추가
     li.classList.add("message"); // message 클래스 추가
     ul.appendChild(li); // 리스트에 아이템 추가
+    input.value = "";
+    scrollToBottom(); // 스크롤을 최하단으로 내림
   }
 });
