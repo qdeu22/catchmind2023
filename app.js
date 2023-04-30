47ae50a079278b6d752f1b25d485e45cc2427f47
qdeu22 <qdeu22@gmail.com>
2023-04-30 20:02:13 +0900

    feat: @@@구글 로그인@@@ 삭제!!!!!

--- app.js
+++ app.js
@@ -8,72 +8,17 @@ const server = http.createServer(app);
 const socketIO = require("socket.io");
 const io = socketIO(server);
 
-var passport = require("./lib/passport");
-var session = require("express-session");
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(
-  //+
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
-
-app.use(passport.initialize()); //+
-app.use(passport.session()); //+
-
 app.use(express.static(path.join(__dirname, "src")));
 
-function isLoggedIn(req, res, next) {
-  return next(); // 임시적으로!
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.render("alert", {
-    message: "로그인이 필요합니다.",
-    url: "/login",
-  });
-}
-
 app.get("/", (req, res) => {
-  const title = "캐치마인드";
-  const isLoggedIn = !!req.user;
-  const username = req.user ? req.user.emails[0].value : null;
-  res.render("index", { title, isLoggedIn, username });
+  res.sendFile(__dirname + "/views/index.html");
 });
-
-app.get("/login", function (req, res, next) {
-  res.render("login", {});
-});
-app.get("/channel", isLoggedIn, function (req, res, next) {
+app.get("/channel", function (req, res) {
   res.sendFile(__dirname + "/views/channel.html");
-});
-
-app.get(
-  "/login/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/fail" }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("/");
-  }
-);
-
-app.get("/logout", isLoggedIn, function (req, res, next) {
-  req.logout(function (err) {
-    if (err) return next(err);
-    res.redirect("/");
-  });
 });
 
 const canvasIO = io.of("/canvas");
