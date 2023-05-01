636ecd3595861d274adba3f52c1cc346c13681ef
qdeu22 <qdeu22@gmail.com>
2023-05-01 15:36:48 +0900

    feat: 게임 카운트 중 게임시작 버튼을 누르면 취소 동작!

--- src/javascript/game.js
+++ src/javascript/game.js
@@ -1,19 +1,29 @@
 // 게임 시작 버튼을 클릭하면 startGame 함수를 실행합니다.
 var start_button = document.getElementById("start-button");
-start_button.addEventListener("click", startGame);
+
+var countdown = null;
+
+start_button.addEventListener("click", function () {
+  if (countdown === null) {
+    // countdown이 null이면, 즉 게임이 시작되지 않은 상태이면 startGame 함수를 실행합니다.
+    startGame();
+  } else {
+    // countdown이 null이 아니면, 즉 이미 게임이 시작된 상태이면 취소합니다.
+    cancelGame();
+  }
+});
 
 // 버튼의 텍스트를 변경하는 함수
 function updateButtonText(count) {
   start_button.innerHTML = `게임시작 카운트 :${count}`;
 }
-
 // 게임 시작 버튼을 클릭할 때 실행되는 함수
 function startGame() {
   let count = 5; // 초기 카운트는 5입니다.
   updateButtonText(count); // 버튼의 텍스트를 5로 변경합니다.
 
   // 1초마다 실행되는 함수
-  const countdown = setInterval(function () {
+  countdown = setInterval(function () {
     count -= 1; // 카운트를 1씩 감소시킵니다.
     updateButtonText(count); // 버튼의 텍스트를 갱신합니다.
 
@@ -30,4 +40,10 @@ function startGame() {
       canvasSocket.emit("game-start");
     }
   }, 1000);
+}
+// 게임 취소 버튼을 클릭할 때 실행되는 함수
+function cancelGame() {
+  clearInterval(countdown); // interval을 멈춥니다.
+  countdown = null; // countdown 변수를 null로 초기화합니다.
+  start_button.innerHTML = "게임 시작"; // 버튼의 텍스트를 초기화합니다.
 }
