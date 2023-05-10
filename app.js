ffbde81787b7acb263bdec1bff9917f0b4f65980
qdeu22 <qdeu22@gmail.com>
2023-05-10 19:17:07 +0900

    chore : 방기능 연결?

--- app.js
+++ app.js
@@ -21,8 +21,48 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-app.get("/channel", function (req, res) {
-  res.sendFile(__dirname + "/views/channel.html");
+
+const rooms = [];
+
+app.get("/room", (req, res) => {
+  res.sendFile(__dirname + "/views/room.html");
+});
+
+app.post("/room/create", (req, res) => {
+  const roomName = req.body.roomName;
+
+  // 방 생성 작업 수행
+  const newRoom = { id: rooms.length + 1, name: roomName };
+  rooms.push(newRoom);
+  console.log(rooms);
+
+  // 생성된 방 정보를 클라이언트에게 응답
+  res.json({
+    success: true,
+    roomId: newRoom.id,
+  });
+});
+
+// /room/:id 라우팅 경로에서 roomId를 매개변수로 사용합니다.
+app.get("/room/:id", (req, res) => {
+  const roomId = req.params.id;
+  var room = rooms.find((room) => {
+    return room.id === parseInt(roomId); // parseInt 꼭하기
+  });
+
+  console.log(room);
+
+  if (room) {
+    // roomId가 배열의 요소로 포함되어 있을 경우
+    res.sendFile(__dirname + "/views/channel.html");
+  } else {
+    // roomId가 배열의 요소로 포함되어 있지 않을 경우
+    res.status(404).json({ error: `방 ${roomId}은 존재하지 않습니다.` });
+  }
+});
+
+app.get("/rooms", (req, res) => {
+  res.json(rooms);
 });
 
 const wordModule = require("./lib/file");
---
+++ src/javascript/room.js
@@ -0,0 +1,88 @@
+const roomModal = document.getElementById("room-modal");
+const roomModalClose = document.getElementById("room-modal-close");
+
+const inputRoomName = document.getElementById("room-name");
+
+var createBtn = document.getElementById("create-room");
+
+createBtn.addEventListener("click", createRoomModal);
+
+function createRoomModal() {
+  roomModal.style.display = "block"; // 모달창 띄우기
+}
+
+roomModalClose.onclick = function () {
+  roomModal.style.display = "none"; // 모달창 닫기
+  inputRoomName.value = "";
+};
+
+var deleteBtn = document.getElementById("delete-room");
+
+deleteBtn.addEventListener("click", deleteRoom);
+
+const roomCreateButton = document.getElementById("room-create-button");
+
+roomCreateButton.addEventListener("click", createRoom);
+
+function createRoom() {
+  const roomName = document.getElementById("room-name").value;
+  // 새로운 목록 요소 생성
+
+  // 서버로 방 생성 요청 보내기
+  fetch("/room/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      roomName: roomName,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success) {
+        // 성공적으로 방이 생성된 경우
+        alert(`새로운 ${roomName} 방을 생성합니다.`);
+
+        const roomList = document.getElementById("room-list");
+
+        const roomListItem = document.createElement("li");
+        const roomLink = document.createElement("a");
+        roomLink.href = `/room/${data.roomId}`;
+        roomLink.textContent = roomName;
+        roomListItem.appendChild(roomLink);
+        roomList.appendChild(roomListItem);
+
+        window.location.href = `/room/${data.roomId}`;
+      } else {
+        // 방 생성에 실패한 경우
+        alert("방 생성에 실패했습니다.");
+      }
+    })
+    .catch((err) => console.error(err));
+
+  // 모달창 닫기
+  roomModal.style.display = "none";
+  inputRoomName.value = "";
+}
+
+fetch("/rooms")
+  .then((res) => res.json())
+  .then((rooms) => {
+    const roomList = document.getElementById("room-list");
+
+    rooms.forEach((room) => {
+      const roomListItem = document.createElement("li");
+      const roomLink = document.createElement("a");
+      roomLink.href = `/room/${room.id}`;
+      roomLink.textContent = room.name;
+      roomListItem.appendChild(roomLink);
+      roomList.appendChild(roomListItem);
+    });
+  })
+  .catch((err) => console.error(err));
+
+function deleteRoom() {
+  // 방 삭제 기능
+  alert("선택한 방을 삭제합니다.");
+}
